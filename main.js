a98c06ed30de57f9b073974969d6fcc5e00e404d
MBaldicera <126473999+MBaldicera@users.noreply.github.com>
2023-11-13 16:33:18 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-function tocaSomRisada(){
-    document.querySelector("#som_tecla_risada").play();   
+function tocaSomAplausos(){
+    document.querySelector("#som_tecla_aplausos").play();   
 }
 
-document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
+listaDeTeclas[2].onclick = tocaSomAplausos
 
+let contador = 0;
+//Estrutura de repetição - Enquanto
+while(contador<9){
+    listaDeTeclas[0].onclick = tocaSomAplausos;
+    contador = 1;
+}
 
